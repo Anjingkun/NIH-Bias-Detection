f2a9c500731b269c848eb1ab8f11924f9607f7df
Anjingkun <90504584+Anjingkun@users.noreply.github.com>
2023-02-28 15:39:32 +0800

    Add files via upload

--- measure_disparity.py
+++ measure_disparity.py
@@ -17,19 +17,34 @@ from pandas import DataFrame
 
 
 class data():
+    '''
+        This class can become the object of every sample.
+
+        Args:
+            pre: the probability of prediction ,like [0.2,0.8]
+            outcome: the ground truth of the sample
+            model_label: the label of model prediction
+            sample_weight: the weight of the sample 
+            protect_feature: the value of protected feature
+    '''
     def __init__(self, pre, outcome, model_label, sample_weight, protect_feature):
-        self.pre = pre
-        self.outcome = outcome
-        self.model_label = model_label
-        self.sample_weight = sample_weight
-        self.protect_feature = protect_feature
+        self.pre = pre   #[0.2,0.8]
+        self.outcome = outcome # 1
+        self.model_label = model_label # 1
+        self.sample_weight = sample_weight # 0.8
+        self.protect_feature = protect_feature # 1 or 0
 
 
 class DataSet():
+    ''' 
+        This class is the contioner of dataset.We can get some characteristics of the dataset
+        Args:
+            data_array: An array composed of data
+    '''
     def __init__(self, data_array):
         self.data_array = data_array
-        self.unprotected_weight = self.num_a_is_0()
-        self.protected_weigth = self.num_a_is_1()
+        self.unprotected_weight = self.num_a_is_0()# sum of unprotected sample'sweight
+        self.protected_weight = self.num_a_is_1()
         self.pre_favourite_unprotect_weight = self.num_y_hat_is_1_a_is_0()
         self.pre_favourite_protect_weight = self.num_y_hat_is_1_a_is_1()
         self.tru_unfavourite_unprotect_weight = self.num_y_is_0_a_is_0()
@@ -48,63 +63,89 @@ class DataSet():
         self.pre_favourite_tru_favourite_protect_weight = (
             self.num_y_hat_is_1_y_is_1_a_is_1()
         )
-
+    '''
+        Return : the sum of all samples' weights
+    '''
+    def sum_weight(self):
+        sum_weight = 0
+        for data in self.data_array:
+            sum_weight = sum_weight + data.sample_weight
+        return sum_weight
+    '''
+        Return : the sum of unprotected samples' weights
+    '''
     def num_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 0:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected samples' weights
+    '''
     def num_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 1:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of unprotected and predicted favourite samples' weights
+    '''
     def num_y_hat_is_1_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 0 and data.model_label == 1:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected and predicted favourite samples' weights
+    '''
     def num_y_hat_is_1_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 1 and data.model_label == 1:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of unprotected and unfavourite samples' weights
+    '''
     def num_y_is_0_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 0 and data.outcome == 0:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected and unfavourite samples' weights
+    '''
     def num_y_is_0_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 1 and data.outcome == 0:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of unprotected and favourite samples' weights
+    '''
     def num_y_is_1_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 0 and data.outcome == 1:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected and favourite samples' weights
+    '''
     def num_y_is_1_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
             if data.protect_feature == 1 and data.outcome == 1:
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of unprotected and predicted favourite but ture unfavourite samples' weights
+    '''
     def num_y_hat_is_1_y_is_0_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
@@ -115,7 +156,9 @@ class DataSet():
             ):
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected and predicted favourite but ture unfavourite samples' weights
+    '''
     def num_y_hat_is_1_y_is_0_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
@@ -126,7 +169,9 @@ class DataSet():
             ):
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of unprotected and predicted favourite and ture favourite samples' weights
+    '''
     def num_y_hat_is_1_y_is_1_a_is_0(self):
         sum_weight = 0
         for data in self.data_array:
@@ -137,7 +182,9 @@ class DataSet():
             ):
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of protected and predicted favourite and ture favourite samples' weights
+    '''
     def num_y_hat_is_1_y_is_1_a_is_1(self):
         sum_weight = 0
         for data in self.data_array:
@@ -148,13 +195,63 @@ class DataSet():
             ):
                 sum_weight = sum_weight + data.sample_weight
         return sum_weight
-
+    '''
+        Return : the sum of true positive samples' weights
+    '''
+    def num_y_hat_and_y_is_1(self):
+        sum_weight = 0
+        for data in self.data_array:
+            if (
+                data.outcome == 1
+                and data.model_label == 1
+            ):
+                sum_weight = sum_weight + data.sample_weight
+        return sum_weight
+    '''
+        Return : the sum of true negivate samples' weights
+    '''
+    def num_y_hat_and_y_is_0(self):
+        sum_weight = 0
+        for data in self.data_array:
+            if (
+                data.outcome == 0
+                and data.model_label == 0
+            ):
+                sum_weight = sum_weight + data.sample_weight
+        return sum_weight
+    '''
+        Return : the sum of positive samples' weights
+    '''
+    def num_y_is_1(self):
+        sum_weight = 0
+        for data in self.data_array:
+            if (
+                data.outcome == 1
+            ):
+                sum_weight = sum_weight + data.sample_weight
+        return sum_weight
+    '''
+        Return : the sum of negivate samples' weights
+    '''
+    def num_y_is_0(self):
+        sum_weight = 0
+        for data in self.data_array:
+            if (
+                data.outcome == 0
+            ):
+                sum_weight = sum_weight + data.sample_weight
+        return sum_weight
+    '''
+        Return : Statistical Parity Difference
+    '''
     def SPD(self):
         return abs(
             self.pre_favourite_unprotect_weight / self.unprotected_weight
-            - self.pre_favourite_protect_weight / self.protected_weigth
+            - self.pre_favourite_protect_weight / self.protected_weight
         )
-
+    '''
+        Return : Average Odds Difference
+    '''
     def AOD(self):
         return (
             abs(
@@ -170,7 +267,9 @@ class DataSet():
                 / self.tru_favourite_protect_weight
             )
         ) / 2
-
+    '''
+        Return : Equal Opportunity Difference
+    '''
     def EDO(self):
         return abs(
             self.pre_favourite_tru_favourite_unprotect_weight
@@ -178,11 +277,20 @@ class DataSet():
             - self.pre_favourite_tru_favourite_protect_weight
             / self.tru_favourite_protect_weight
         )
-
+    '''
+        Return : ths true positive rate
+    '''
+    def true_positive_rate(self):
+        return self.num_y_hat_and_y_is_1()/self.num_y_is_1()
+    '''
+        Return : ths true negitive rate
+    '''
+    def true_negetive_rate(self):
+        return self.num_y_hat_and_y_is_0()/self.num_y_is_0()
 
 def measure_disparity(data_frame):
     data_array = []
-    for index, row in data_frame.iterrows():
+    for index, row in data_frame.iterrows():#
         this_data = data(
             eval(row["Model prediction"]),
             row["Binary outcome"],
@@ -195,3 +303,61 @@ def measure_disparity(data_frame):
     print("This dataset's SPD is " + str(dataset.SPD()))
     print("This dataset's AOD is " + str(dataset.AOD()))
     print("This dataset's EOD is " + str(dataset.EDO()))
+    spd = []
+    aod = []
+    eod = []
+    balence_rate=[]
+    thresh_arr = np.linspace(0.01, 1, 100)
+    
+    for thresh in thresh_arr:
+        data_array = []
+        for index, row in data_frame.iterrows():
+            prediction=eval(row["Model prediction"])
+            outcome=row["Binary outcome"]
+            label=row["Model label"]
+            weights=row["Sample weights"]
+            protected=row["Demographic data on protected and reference classes"]
+            if prediction[1]>=thresh:
+                label=1
+            else:
+                label=0
+            this_data = data(prediction,outcome,label,weights,protected)
+            data_array.append(this_data)
+        dataset = DataSet(data_array)
+        spd.append(dataset.SPD())
+        aod.append(dataset.AOD())
+        eod.append(dataset.EDO())
+        balence_rate.append((dataset.true_negetive_rate()+dataset.true_positive_rate())/2)
+    plot(thresh_arr, 'Classification Thresholds',
+     balence_rate, 'Balanced Accuracy',
+     spd, 'SPD')
+    plot(thresh_arr, 'Classification Thresholds',
+     balence_rate, 'Balanced Accuracy',
+     aod, 'AOD')
+    plot(thresh_arr, 'Classification Thresholds',
+     balence_rate, 'Balanced Accuracy',
+     eod, 'EOD')
+    
+def plot(x, x_name, y_left, y_left_name, y_right, y_right_name):
+    fig, ax1 = plt.subplots(figsize=(10,7))
+    ax1.plot(x, y_left)
+    ax1.set_xlabel(x_name, fontsize=16, fontweight='bold')
+    ax1.set_ylabel(y_left_name, color='b', fontsize=16, fontweight='bold')
+    ax1.xaxis.set_tick_params(labelsize=14)
+    ax1.yaxis.set_tick_params(labelsize=14)
+    ax1.set_ylim(0.5, 0.8)
+
+    ax2 = ax1.twinx()
+    ax2.plot(x, y_right, color='r')
+    ax2.set_ylabel(y_right_name, color='r', fontsize=16, fontweight='bold')
+    if 'DI' in y_right_name:
+        ax2.set_ylim(0., 0.7)
+    else:
+        ax2.set_ylim(0, 0.7)
+
+    best_ind = np.argmax(y_left)
+    ax2.axvline(np.array(x)[best_ind], color='k', linestyle=':')
+    ax2.yaxis.set_tick_params(labelsize=14)
+    ax2.grid(True)
+
+
--- mitigate_disparity.py
+++ mitigate_disparity.py
@@ -12,13 +12,24 @@ from aif360.metrics import utils
 
 
 class BiasRemoverModel:
+    '''
+        BiasRemoverModel is a model that can be trained by a dataset.Before using this model ,
+        the dataset will be reweighted by MultiLevelReweighing. After reweighing the dataset, 
+        we can train the model.
+    '''
     def __init__(self):
         self.lr_model = make_pipeline(
             StandardScaler(), LogisticRegression(solver="liblinear", random_state=1)
         )
         
         
-
+    '''
+        This function can train the model.
+        Args:
+            dataset (BinaryLabelDataset): Dataset containing true labels.
+        Return:
+            BiasRemoverModel (self)
+    '''
     def fit(self, dataset):
         privileged_groups=[]
         unprivileged_groups=[]
@@ -33,34 +44,46 @@ class BiasRemoverModel:
                 'unprivileged_value':dataset.unprivileged_protected_attributes[i][0],
                 'level':1                           #we can change this level
             })
-        rw = AjkReweighing(unprivileged_groups,privileged_groups)
+        rw = MultiLevelReweighing(unprivileged_groups,privileged_groups)
         trans_dataset = rw.fit_transform(dataset)
-        fit_params = {'logisticregression__sample_weight': dataset.instance_weights}
+        fit_params = {'logisticregression__sample_weight': trans_dataset.instance_weights}
         lr_trans_dataset_model = self.lr_model.fit(dataset.features, dataset.labels.ravel(), **fit_params)
         self.lr_model=lr_trans_dataset_model
+        return self
+    '''
+        This function can output the Probability of prediction
+        Args:
+            features (numpy):ths features of samples.
+            such as :[
+            [1,1,0,1,0],
+            [1,1,0,0,0]
+            ]
+        Return :
+            the Probability of prediction
+    '''
     def predic_prob(self,features):
         return self.lr_model.predict_proba(features)
         
         
 
 
-class AjkReweighing(Transformer):
-    """Reweighing is a preprocessing technique that Weights the examples in each
+class MultiLevelReweighing(Transformer):
+    """MultiLevelReweighing is a preprocessing technique that Weights the examples in each
     (group, label) combination differently to ensure fairness before
-    classification [4]_.
-
+    classification .This technique can compute the protected level of every sample , and then it can 
+    give every sample a new weight.
     """
 
     def __init__(self, unprivileged_groups, privileged_groups):
         """
         Args:
-            unprivileged_groups (list(dict)): Representation for unprivileged
-                group.
+            unprivileged_groups (list(dict)): Representation for unprivileged group.
             privileged_groups (list(dict)): Representation for privileged group.
-            [{'feature_name':'sex','privileged_value':1,'level':1},
+            such as:
+            [{'feature_name':'sex','privileged_value':1,'level':2},
             {'feature_name':'race','privileged_value':1,'level':1}]
         """
-        super(AjkReweighing, self).__init__(
+        super(MultiLevelReweighing, self).__init__(
             unprivileged_groups=unprivileged_groups, privileged_groups=privileged_groups
         )
 
@@ -79,7 +102,7 @@ class AjkReweighing(Transformer):
             dataset (BinaryLabelDataset): Dataset containing true labels.
 
         Returns:
-            Reweighing: Returns self.
+            MultiLevelReweighing: Returns self.
         """
 
         (
@@ -94,7 +117,8 @@ class AjkReweighing(Transformer):
             level_max = level_max + group["level"]
         n = np.sum(dataset.instance_weights, dtype=np.float64)
         n_p_level_dic = {}
-        
+        #weight:[1,2,1,1,2]
+        #'n_p1':2
         for level in range(0, level_max + 1):
             n_p_level_dic["n_p" + str(level)] = np.sum(
                 dataset.instance_weights[
@@ -107,6 +131,7 @@ class AjkReweighing(Transformer):
         n_unfav = np.sum(dataset.instance_weights[unfav_cond], dtype=np.float64)
 
         n_p_level_fav_or_unfav_dic = {}
+        #"n_p1_unfav":3
         for level in range(0, level_max + 1):
             n_p_level_fav_or_unfav_dic["n_p" + str(level) + "_fav"] = np.sum(
                 dataset.instance_weights[
@@ -124,6 +149,8 @@ class AjkReweighing(Transformer):
         self.w_p_level_fav_or_unfav_dic = {}
 
         # reweighing weights
+        #dataset.instance_weights
+        #"w_p3_fav":3
         for level in range(0, level_max + 1):
             self.w_p_level_fav_or_unfav_dic["w_p" + str(level) + "_fav"] = (
                 n_fav
@@ -178,7 +205,7 @@ class AjkReweighing(Transformer):
     def _obtain_conditionings(self, dataset):
         """Obtain the necessary conditioning boolean vectors to compute
         instance level weights.
-        [{'feature_name':'sex','privileged_value':1,'level':1},
+        [{'feature_name':'sex','privileged_value':1,'level':2},
             {'feature_name':'race','privileged_value':1,'level':1}]
         """
         # conditioning
@@ -191,8 +218,16 @@ class AjkReweighing(Transformer):
             protect_level_dic["protect_level" + str(i) + "_cond"] = np.zeros(
                 dataset.protected_attributes.shape[0], dtype=bool
             )
-
-        for sample_index in range(len(dataset.protected_attributes)):
+        #'protect_level3_cond':[0,1,0]
+        #'protect_level2_cond':[0,0,1]
+        # [
+        #     [1,1],
+        #     [0,1],
+        #     [1,0],
+        #     [0,0]
+        # ]
+        # ['sex','race']
+        for sample_index in range(len(dataset.protected_attributes)):#3,5
             protect_feature = dataset.protected_attributes[sample_index]
             this_sample_protect_level = 0
             for group in self.privileged_groups:
@@ -201,17 +236,22 @@ class AjkReweighing(Transformer):
                 level = group["level"]
                 index = dataset.protected_attribute_names.index(name)
                 if protect_feature[index] == val:
-                    this_sample_protect_level = this_sample_protect_level + level
+                    this_sample_protect_level = this_sample_protect_level + level# 3
             protect_level_dic[
                 "protect_level" + str(this_sample_protect_level) + "_cond"
             ][sample_index] = True
-
-        fav_cond = dataset.labels.ravel() == dataset.favorable_label
-        unfav_cond = dataset.labels.ravel() == dataset.unfavorable_label
+            # "protect_level" + str(3) + "_cond":[0,0,0,1,0]
+        
+        #[[1],[0],[1]],[1,0,1]
+        fav_cond = (dataset.labels.ravel() == dataset.favorable_label) #[0,0,1,1,0]
+        unfav_cond = (dataset.labels.ravel() == dataset.unfavorable_label)#[1,1,0,0,1]
 
         # combination of label and privileged/unpriv. groups
         combination_p_fav_dic = {}
-        for i in range(0, level_max + 1):
+        #"cond_p3_fav":[0,0,1,0,0]
+
+        for i in range(0, level_max + 1):#3
+
             combination_p_fav_dic["cond_p" + str(i) + "_fav"] = np.logical_and(
                 fav_cond, protect_level_dic["protect_level" + str(i) + "_cond"]
             )
